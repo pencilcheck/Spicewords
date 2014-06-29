5c607719e1f6195ff17edf210a1887127bab997f
Penn Su <penn@gameyourgame.com>
2014-06-29 13:32:56 -0700

    Add test page

--- www/js/app.js
+++ www/js/app.js
@@ -75,6 +75,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           controller: 'PlaylistCtrl'
         }
       }
+    })
+
+    .state('app.test', {
+      url: "/pennsu",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/pennsu.html"
+        }
+      }
     });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/message');
